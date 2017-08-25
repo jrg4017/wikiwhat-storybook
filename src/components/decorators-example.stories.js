68171290f1662f918a9c034645681d0d0a8505b0
Julianna Gabler <julgable@usnycsrf3254.global.publicisgroupe.net>
2017-08-25 09:59:42 -0400

    initial commit

---
+++ src/components/decorators-example.stories.js
@@ -0,0 +1,17 @@
+// way to wrap a common set of components.
+
+//ex: might want to center all stories, wrap it and do so
+// import React from 'react';
+// import { storiesOf } from '@storybook/react';
+// import MyComponent from '../my_component';
+//
+// storiesOf('MyComponent', module)
+//   .addDecorator(story => (
+//     <div style={{textAlign: 'center'}}>
+//       {story()}
+//     </div>
+//   ));
+//
+//   configure(function() {
+//
+//   }, module);
