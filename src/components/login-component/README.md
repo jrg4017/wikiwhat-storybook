f05d7ecf34cb91751a66ebfbc32277e979fcfa6d
Anna Wen <annwen1@usnycsrf9713.global.publicisgroupe.net>
2017-09-05 11:50:12 -0400

    skit

---
+++ src/components/login-component/README.md
@@ -0,0 +1,12 @@
+# Login Component
+
+Font:
+Roboto
+
+Button Color: 
+- ![#fcbe32](https://placehold.it/15/fcbe32/000000?text=+) '#fcbe32'
+
+Possible background colors:
+ - ![#f8f8f8](https://placehold.it/15/f8f8f8/000000?text=+) `#f8f8f8`
+ - ![#C0C0C0](https://placehold.it/15/C0C0C0/000000?text=+) `#C0C0C0`
+ - ![#A9A9A9](https://placehold.it/15/A9A9A9/000000?text=+) `#A9A9A9`
---
+++ src/stories/skit.stories.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import withReadme from 'storybook-readme/with-readme';
+import backgrounds from '@storybook/addon-backgrounds';
+import { withKnobs, text, boolean, number, color, select } from '@storybook/addon-knobs';
+
+import LoginReadMe from '../components/login-component/README.md';
+import Login from '../components/login-component/Login';
+import ButtonReadme from '../components/test-button/README.md';
+
+/**
+  * const for declarators - wrap entire content with formatting etc
+  */
+const styles = {
+  textAlign: 'center',
+};
+const CenterDecorator = (storyFn) => (
+  <div style={styles}>
+    { storyFn() }
+  </div>
+);
+
+/**
+  * stories displaying different features
+  */
+storiesOf('UI testing', module)
+  .addDecorator(CenterDecorator)
+  .addDecorator(withKnobs)
+  .addDecorator(backgrounds([
+    { name: 'white', value: '#FFFFFF', default: true },
+    { name: 'light gray', value: '#e7e7e7' },
+    { name: 'dark gray', value: '#A9A9A9' }
+  ]))
+
+  // Notes Addon
+  .add('login component', withReadme(LoginReadMe,
+    () => {
+    const login = text('login', 'Please login to keeep trck of your score');
+
+    const btnColor = color('Button Color', '#ff00ff');
+
+    return (
+      <div>
+        {login}
+        <form className="form1">
+          <input type='text' placeholder='Username' required />
+          <input type='text' placeholder='Password' required />
+          <button style={{backgroundColor: btnColor}} type='submit'>Login</button>
+
+          <div className="register-text1">
+            <a href=' '>Don't have an account? Create one!</a>
+          </div>
+        </form>
+      </div>
+
+    );
+  }))
